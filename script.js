687549590c7e36ba306fc57c5795001811393f21
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-27 11:12:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function curry(callback) {
-  
+  function curriedFn(...args){
+	  if(args.length >= callback.length){
+		  return callback(...args);
+	  }else{
+		  return (...moreArgs) => curriedFn(...args, ...moreArgs);
+	  }
+  }
+	return curriedFn;
 }
 
-module.export=curry
+module.export=curry;
